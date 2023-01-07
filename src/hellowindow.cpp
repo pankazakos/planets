618b3f9990d982a233102078ec96348411e7b55e
pankazakos <pankazakos01@gmail.com>
2023-01-07 18:47:09 +0200

    Removed buffers and textures

--- src/hellowindow.cpp
+++ src/hellowindow.cpp
@@ -63,63 +63,6 @@ int main()
     return -1;
   }
 
-  // clang-format off
-  float vertices[]{
-    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f,  // bottom left
-    0.5f, -0.5f, 0.0f,   1.0f, 0.0f,  // bottom right
-    0.5f,  0.5f, 0.0f,   1.0f, 1.0f,  // top right
-    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f,  // bottom left
-    -0.5f,  0.5f, 0.0f,   0.0f, 1.0f,  // top left
-    0.5f,  0.5f, 0.0f,   1.0f, 1.0f,  // top right
-  };
-  // clang-format on
-
-  // Vertex Buffer Object
-  unsigned int VBO;
-  glGenBuffers(1, &VBO);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  // Vertex Array Object
-  unsigned int VAO;
-  glGenVertexArrays(1, &VAO);
-  glBindVertexArray(VAO);
-
-  // position attribute
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
-
-  // texture coord attribute
-  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
-
-  // load and create a texture
-  // -------------------------
-  unsigned int texture;
-  glGenTextures(1, &texture);
-  glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
-  // set the texture wrapping parameters
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  // set texture filtering parameters
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-  int width, height, nrChannels;
-  unsigned char *data = stbi_load("/home/pank/Documents/7o_eks/graphics/LearnOpenGL/resources/textures/block_solid.png", &width, &height, &nrChannels, 0);
-
-  if (data)
-  {
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-  }
-  else
-  {
-    std::cout << "Failed to load texture" << std::endl;
-  }
-  stbi_image_free(data);
-
   // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
   stbi_set_flip_vertically_on_load(true);
 
@@ -156,10 +99,6 @@ int main()
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // bind textures on corresponding texture units
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
     // render the triangle
     myShader.use();
 
@@ -187,11 +126,6 @@ int main()
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
-
-  // de-allocate all resources once they've outlived their purpose:
-  // ------------------------------------------------------------------------
-  glDeleteVertexArrays(1, &VAO);
-  glDeleteBuffers(1, &VBO);
 
   glfwTerminate();
 }
